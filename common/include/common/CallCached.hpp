87d0dec28185f440115e9d3feddd17448c0760f1
Alexander Akhundzhanov <kerrytazi@mail.ru>
2026-01-20 09:12:17 +0300

    Added CallCached

---
+++ common/include/common/CallCached.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <type_traits>
+#include <optional>
+
+template <typename TFunc>
+class CallCachedStorage
+{
+public:
+
+	using value_type = std::remove_reference_t<std::invoke_result_t<TFunc>>;
+
+	value_type& Deref()
+	{
+		CheckInit();
+		return *value_;
+	}
+
+private:
+
+	void CheckInit()
+	{
+		if (!value_.has_value())
+			value_ = TFunc{}();
+	}
+
+	std::optional<value_type> value_;
+};
+
+template <typename TFunc>
+auto CallCached()
+{
+	CallCachedStorage<TFunc> storage;
+	return storage.Deref();
+}
